4f06ac648d1d1e5057231730973d835caf24d9a8
yeasinmiah1272001 <137316520+yeasinmiah1272001@users.noreply.github.com>
2025-03-21 22:38:07 +0600

    user updated success

--- src/pages/dashboard/admin/ManageUser.jsx
+++ src/pages/dashboard/admin/ManageUser.jsx
@@ -1,17 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
+
+import toast from "react-hot-toast";
+import UserUpdateModal from "../../../components/Modal/UserUpdateModal";
 
 const ManageUser = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedUser, setSelectedUser] = useState(null);
   const axiosSecure = useAxiosSecure();
 
-  const { data: users = [] } = useQuery({
+  const { data: users = [], refetch } = useQuery({
     queryFn: async () => {
       const { data } = await axiosSecure.get("/users");
       return data;
     },
     queryKey: ["users"],
   });
+
+  const { mutateAsync } = useMutation({
+    mutationFn: async (updatedUser) => {
+      const { data } = await axiosSecure.patch(
+        `/user/update/${updatedUser.email}`,
+        updatedUser
+      );
+      return data;
+    },
+    onSuccess: () => {
+      toast.success("User updated successfully");
+      refetch();
+      setIsOpen(false);
+    },
+    onError: () => {
+      toast.error("Failed to update user");
+    },
+  });
+
+  const handleUpdateRole = (user) => {
+    setSelectedUser(user);
+    setIsOpen(true);
+  };
+
+  const modalHandler = async (selectedRole) => {
+    if (!selectedUser) return;
+
+    const updatedUser = {
+      email: selectedUser.email,
+      role: selectedRole,
+      status: "verified",
+    };
+
+    try {
+      await mutateAsync(updatedUser);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <div className="p-4">
@@ -22,28 +66,31 @@ const ManageUser = () => {
               <th className="border border-gray-300 px-4 py-2">Email</th>
               <th className="border border-gray-300 px-4 py-2">Role</th>
               <th className="border border-gray-300 px-4 py-2">Status</th>
-              <th className="border border-gray-300 px-4 py-2">Action</th>
-              <th className="border border-gray-300 px-4 py-2">Action</th>
+              <th className="border border-gray-300 px-4 py-2">Update Role</th>
+              <th className="border border-gray-300 px-4 py-2">Remove User</th>
             </tr>
           </thead>
-          <tbody className="border cursor-pointer ">
+          <tbody className="border">
             {users.map((user, index) => (
               <tr key={index} className="text-center">
                 <td className="border border-gray-300 px-4 py-2">
                   {user.email}
                 </td>
                 <td className="border border-gray-300 px-4 py-2">
-                  {user.role}
+                  {user.role.charAt(0).toUpperCase() + user.role.slice(1)}
                 </td>
                 <td className="border border-gray-300 px-4 py-2">
-                  <span className="text-white bg-green-400 p-1 font-medium rounded-md">
+                  <span className="text-white bg-green-400 px-2 py-1 font-medium rounded-md">
                     {user.status}
                   </span>
                 </td>
                 <td className="border border-gray-300 px-4 py-2">
-                  <span className="text-white bg-green-400 p-1 font-medium rounded-md">
+                  <button
+                    onClick={() => handleUpdateRole(user)}
+                    className="relative inline-block px-3 py-1 font-semibold text-green-900 bg-green-200 rounded-full hover:bg-green-300 transition"
+                  >
                     Update Role
-                  </span>
+                  </button>
                 </td>
                 <td className="border border-gray-300 px-4 py-2">
                   <button
@@ -58,6 +105,15 @@ const ManageUser = () => {
           </tbody>
         </table>
       </div>
+
+      {isOpen && (
+        <UserUpdateModal
+          setIsOpen={setIsOpen}
+          isOpen={isOpen}
+          modalHandler={modalHandler}
+          user={selectedUser}
+        />
+      )}
     </div>
   );
 };
